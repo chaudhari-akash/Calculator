a3f3ba4485d91024f15d7c40021b4a7042946143
akash <akash.chaudhari.1977@gmail.com>
2025-02-26 22:08:37 +0530

    Added Functions

--- src/main/java/spe/Main.java
+++ src/main/java/spe/Main.java
@@ -2,6 +2,26 @@ package spe;
 import java.util.Scanner;
 
 public class Main {
+
+    public static double addition(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return a / b;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -26,16 +46,16 @@ public class Main {
             num2 = scanner.nextDouble();
 
             switch (choice) {
-                case 1 -> System.out.println("Result: " + (num1 + num2));
-                case 2 -> System.out.println("Result: " + (num1 - num2));
-                case 3 -> System.out.println("Result: " + (num1 * num2));
-                case 4 -> {
-                    if (num2 != 0) System.out.println("Result: " + (num1 / num2));
-                    else System.out.println("Error! Division by zero.");
-                }
+                case 1 -> System.out.println("Result: " + addition(num1 , num2));
+                case 2 -> System.out.println("Result: " + subtract(num1 , num2));
+                case 3 -> System.out.println("Result: " + multiply(num1 , num2));
+                case 4 -> System.out.println("Result: " + divide(num1 , num2));
                 default -> System.out.println("Invalid choice. Please try again.");
             }
         }
         scanner.close();
     }
+
+
+
 }
